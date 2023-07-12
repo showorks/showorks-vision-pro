f8a5ad34d63127f954ce27a23bb0edcc8439f0f4
Lokesh Sehgal <lastw0nd3r@gmail.com>
2023-07-12 15:52:33 +0530

    Initial Checkin of SwiftUI codebase

---
+++ ShoWorks/ShoWorks/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  ShoWorks
+//
+//  Created by Lokesh on 12/07/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+//        NavigationSplitView {
+//            List {
+//                Text("Item")
+//            }
+//            .navigationTitle("Sidebar")
+//        } detail: {
+            VStack {
+                Text("Initial Scene")
+            }
+            .navigationTitle("ShoWorks")
+            .padding()
+//        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoWorks/ShoWorks/ShoWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoWorksApp.swift
+//  ShoWorks
+//
+//  Created by Lokesh on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
