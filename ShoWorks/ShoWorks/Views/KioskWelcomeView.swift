428d2501dd22761359b3701ce005be6b3cae0fdd
Lokesh Sehgal <lastw0nd3r@gmail.com>
2023-08-18 01:22:58 +0530

    Logical changes on welcome kiosk screen

--- ShoWorks/ShoWorks/Views/KioskWelcomeView.swift
+++ ShoWorks/ShoWorks/Views/KioskWelcomeView.swift
@@ -10,18 +10,39 @@ import SwiftUI
 struct KioskWelcomeView: View {
 
     @EnvironmentObject var viewModel: KioskViewModel
-
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
         NavigationStack {
             ZStack {
                 CustomBackground()
                     .edgesIgnoringSafeArea(.all)
                 VStack{
+                    HStack(){
+                        Spacer()
+                        
+                        Button(action: {
+                              
+                            presentationMode.wrappedValue.dismiss()
+                            
+                           }, label: {
+                               Image(systemName: "multiply.circle")
+                                   .resizable(resizingMode: .stretch)
+                                   .foregroundColor(.white)
+                        })
+                        .foregroundColor(Color.white)
+                        .frame(width: 50.0, height: 50.0)
+                        .padding(.trailing, 50)
+                        .padding(.top, 50)
+                        .buttonStyle(PlainButtonStyle())
+                        
+                    }
                     Text(SheetUtility.sharedInstance.getFairNameOfCurrentSheet(sheetDic: viewModel.selectedDictionary)).font(.heleveticNeueMedium(size: 50))
                         .foregroundColor(.white)
                         .padding(.leading,50)
-                    Spacer().frame(height: 60)
-                    ShoWorksLogo()
+                    Spacer().frame(height: 40)
+                    Image("SplashLogo")
+                        .resizable()
+                        .frame(width: 150, height: 170, alignment: .center)
                     
                     Button(action: {
                           
@@ -29,22 +50,24 @@ struct KioskWelcomeView: View {
                        }, label: {
                            VStack{
                                Text("tap_here_to".localized())
-                                   .font(.system(size: 42))
-                                   .font(.heleveticNeueMedium(size: 50))
+                                   .font(.system(size: 36))
+                                   .font(.heleveticNeueMedium(size: 36))
+                                   .foregroundColor(Color.white)
                                Text("check_in_txt".localized())
-                                   .font(.system(size: 70))
-                                   .font(.heleveticNeueBold(size: 50))
+                                   .font(.system(size: 55))
+                                   .font(.heleveticNeueBold(size: 55))
+                                   .foregroundColor(Color.white)
                            }
                            .padding(.trailing,250)
                            .padding(.leading,250)
-                           .padding(.top,30)
-                           .padding(.bottom,30)
+                           .padding(.top,20)
+                           .padding(.bottom,20)
                            .cornerRadius(5)
                     })
                     .cornerRadius(5)
                     .foregroundColor(Color.white)
                     .background(Color.aBlueBackgroundColor)
-                    .padding(.init(top: 30, leading: 50, bottom: 30, trailing: 50))
+                    .padding(.init(top: 20, leading: 40, bottom: 20, trailing: 40))
                     .cornerRadius(5)
                     .buttonStyle(PlainButtonStyle())
                     
