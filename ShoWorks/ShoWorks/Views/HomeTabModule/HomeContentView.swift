39614b2acc079741ede7bc2a2e0d7161daac970b
Lokesh Sehgal <lastw0nd3r@gmail.com>
2023-10-31 01:35:52 +0530

    Layout changes and added help text

--- ShoWorks/ShoWorks/Views/HomeTabModule/HomeContentView.swift
+++ ShoWorks/ShoWorks/Views/HomeTabModule/HomeContentView.swift
@@ -52,7 +52,7 @@ struct HomeContentView: View {
     @State var selectedTab: Tabs = .tab1
     @State private var offsetY: CGFloat = 0
     @State private var isDragging = false
-    
+    @State private var isDeviceConnected = false
     var body: some View {
         
         ZStack{
@@ -89,7 +89,11 @@ struct HomeContentView: View {
                             .frame(width: 960, height: 540)
                             .glassBackgroundEffect()
                         
-                        QRScanTabView()
+                        if isDeviceConnected {
+                            QRScanTabView()
+                        }else{
+                            QRScanDisconnectedTabView()
+                        }
                     }
                 }else{
                     ZStack{
@@ -97,7 +101,15 @@ struct HomeContentView: View {
                             .frame(width: 960, height: 540)
                             .glassBackgroundEffect()
                         
-                        Text("Place tab 3 here")
+                        VStack{
+                            ScrollView {
+                                HStack{}.frame(height: 6)
+                                
+                                
+                            }
+                            .scrollIndicators(.never)
+                            .padding(.vertical, 6)
+                        }
                     }
                 }
                 
@@ -124,8 +136,71 @@ struct QRScanTabView: View {
               
                 Text("Device is connected and ready to scan")
                     .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .font(.system(size: 30))
+                    .padding(.top,40)
                
                 
+            })
+                        
+        }
+    }
+}
+
+
+struct QRScanDisconnectedTabView: View {
+    
+    var body: some View {
+        ZStack(){
+            
+            VStack(alignment: .center, content: {
+                
+               
+                ScrollView(){
+                    Image("connected_state")
+                        .padding(.top,60)
+                  
+                    Divider()
+                        .background(Color.white)
+                        .frame(width: 960)
+                        .padding(.top,20)
+                   
+                    Text("How to connect your scanner device via Bluetooth ?")
+                        .padding(.top,20).font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .frame(alignment: .leading)
+                    
+                    Text("1. Go to the Bluetooth Connection Instruction Manual. \n2. Press scanner's button to power on the scanner. \n3. Bluetooth HID mode is usually set by default on new devices. ShoWorks uses the newer BLE mode for more efficient and reliable data communications and you must change your device to this mode by scanning the code below or found in your instruction manual. \n4. Make sure the device is configured to use Bluetooth mode instead of USB. Please scan the following barcode to allow the device to work with Bluetooth mode.")
+                        .padding(.top,20).font(.system(size: 15))
+                        .frame(width: 860)
+                        .frame(alignment: .leading)
+                    
+                    Image("netum_bluetooth")
+                        .padding(.top,20)
+                    Text("5. BLE Mode Serial Port Profile- For iOS Devices By scanning below barcode scanner will enter BLE Mode for ShoWorks iPad app.")
+                        .padding(.top,20).font(.system(size: 15))
+                        .frame(width: 860)
+                        .frame(alignment: .leading)
+                    
+                    Image("netum_scan")
+                    
+                    Text("6. Once the above barcode has been scanned with the Bluetooth scanner, it will automatically connect with the ShoWorks iPad app each time you start the app, however this initial setup requires that you force close (\"kill\") the app after the configuration has been made for the first time. See iPad/iOS documentation on how to force close apps. \n7. If the Bluetooth scanner has been successfully paired with the iPad prior and is active, a beep sound and brief visual message will occur when the ShoWorks app is launched, indicating that the two devices are now properly communicating. \n\nTROUBLESHOOTING: If the two devices do not seem to be communicating, try the following: \n\n - Force close the ShoWorks app and relaunch (See iPad/iOS documentation on how to force close apps). \n - Perform a hard reset on the Bluetooth scanner. The barcode below will hard reset your scanner. Once a hard reset has been made, you must again scan the two other barcodes with the scanner to \"Work with Bluetooth\" and \"BLE Mode\" (AT+MODE=3). After this has been done, you must force close the ShoWorks app and relaunch.")
+                        .padding(.top,20).font(.system(size: 15))
+                        .frame(width: 860)
+                        .frame(alignment: .leading)
+                    
+                    Text("\n\n From your Vision Pro...")
+                        .padding(.top,20).font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .frame(alignment: .leading)
+                    
+                    Text("\n1. Go to the Settings app on the Vision Pro \n2. Tap Bluetooth \n3. Make sure to turn it on. If you do not see your handheld scanner in the list, make sure that the Bluetooth is turned on and also you have turned on the scanning device. \n4. Go to the ShoWorks Vision Pro app")
+                        .padding(.top,20).font(.system(size: 15))
+                        .frame(width: 860)
+                        .frame(alignment: .leading)
+                }
+                .frame(width: 960,height: 530)
+           
             })
                         
         }
