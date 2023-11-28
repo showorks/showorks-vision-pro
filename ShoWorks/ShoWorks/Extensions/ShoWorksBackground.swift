658cd1214c5d9dd4cf912300f05fb46530f5d78a
Lokesh Sehgal <lastw0nd3r@gmail.com>
2023-11-28 14:52:07 +0530

    Introduced new backgrounds and stuff

--- ShoWorks/ShoWorks/Extensions/ShoWorksBackground.swift
+++ ShoWorks/ShoWorks/Extensions/ShoWorksBackground.swift
@@ -14,54 +14,54 @@ struct ShoWorksBackground: View {
             switch(UserSettings.shared.roundRobinBackgroundImageIndex){
             case 0:
                 
-                Image("background_new_1")
+                Image("background_new_8")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                 
             case 1:
                 
-                Image("background_new_2")
+                Image("background_new_9")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
             case 2:
                 
-                Image("background_new_3")
+                Image("background_new_10")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
             case 3:
                 
-                Image("background_new_4")
+                Image("background_new_11")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
             case 4:
                 
-                Image("background_new_5")
+                Image("background_new_12")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
 
             case 5:
-                Image("background_new_6")
+                Image("background_new_13")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                 
             case 6:
-                Image("background_new_7")
+                Image("background_new_14")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
             case .none:
-                Image("background_new_4")
+                Image("background_new_15")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
             case .some(_):
-                Image("background_new_4")
+                Image("background_new_3")
                     .resizable(resizingMode: .stretch)
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
